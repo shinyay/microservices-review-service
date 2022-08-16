930190c7d958383b4c5afc3e5a223088352bf07e
shinyay <shinya.com@gmail.com>
2022-08-16 14:54:59 +0900

    Initial Commit

---
+++ src/main/kotlin/io/spring/shinyay/microservices/review/ReviewServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.spring.shinyay.microservices.review
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReviewServiceApplication>(*args)
+}
